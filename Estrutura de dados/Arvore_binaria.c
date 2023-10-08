d7917629a199a00b33f0050893f4921d332db917
Gustavo Rebelo <gustavo.rblo@gmail.com>
2023-10-08 17:31:45 -0300

    LingProg

--- Estrutura de dados/Arvore_binaria.c
+++ Estrutura de dados/Arvore_binaria.c
@@ -14,3 +14,23 @@ typedef struct aux{
 
 }NO;
 typedef NO* PONT;
+
+PONT inicializa(){
+    retun(NULL);
+}
+
+PONT adiciona(PONT raiz, PONT no){
+    if (raiz == NULL) return(no);
+    if (no->chave < raiz->chave)
+        raiz->esq = adiciona(raiz->esq, no);
+    else
+        raiz->dir = adiciona(raiz->dir, no);
+        //ignora chave igual
+    return(raiz);
+}
+int main(){
+    PONT r = inicializa();
+
+    no = criaNovoNo(12);
+    r = adiciona(r, no);
+}
---
+++ Linguagem de programação/Refaz.c
@@ -0,0 +1,42 @@
+#define ex11
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#ifdef ex11
+/*Escreva um programa que receba via teclado um tempo em segundos e converta
+    para horas, minutos e segundos.
+    Ex.: recebe 3850 segundos que converte para 1h 4min 10s.*/
+
+
+#endif // ex11
+
+#ifdef ex12
+/*Escreva um programa que receba dados via teclado para variaveis do tipo short,
+    long, int, float, double e um char e imprima-os no video no seguinte
+    formato:
+
+             10        20        30        40        50        60
+     12345678901234567890123456789012345678901234567890123456789012345
+         short               long                int
+                   float               double              char */
+#endif // ex12
+
+#ifdef ex13
+/*Escreva um programa que determine o menor multiplo de um numero inteiro. Este
+    multiplo deve ser maior que o limite minimo recebido. Recebe os 2 numeros via
+    teclado.
+    Ex: menor multiplo de 13 maior que 100. Resultado: 104. */
+#endif // ex13
+
+#ifdef ex14
+/*Escreva um programa que permute o conteúdo de duas variáveis sem utilizar uma
+    variável auxiliar. */
+#endif // ex14
+
+#ifdef ex15
+/*Faça um programa para ler um numero inteiro, positivo de tres digitos, e gerar
+    outro número formado pelos dígitos invertidos do número lido.
+    Ex:   NúmeroLido = 123
+        NúmeroGerado = 321 */
+#endif // ex15
