3efddb4ce6f8abd55202ecc0099d5ef2d5f56a3b
Gustavo Rebelo <gustavo.rblo@gmail.com>
2024-03-04 21:03:13 -0300

    Aula 3 poo

--- Poo/Aula3/src/aula3/Moeda.java
+++ Poo/Aula3/src/aula3/Moeda.java
@@ -1,5 +1,28 @@
 package aula3;
 
 public class Moeda {
-
+	double valor;
+	Correncia correncia;
+	
+	Moeda(double valor, Correncia correncia){
+		this.valor = valor;
+		this.correncia = correncia;
+		
+	}
+	void converterReal() {
+		switch(correncia) {
+		case USD:
+			System.out.println(4.95*valor + "Reais");
+			break;
+		case BTC:
+			System.out.println(336437.80*valor +"reais");
+			break;
+		case EUR:
+			System.out.println(5.37*valor +"reais");
+			break;
+		case BRL:
+			System.out.println(valor +"reais");
+			break;
+		}
+	}
 }
