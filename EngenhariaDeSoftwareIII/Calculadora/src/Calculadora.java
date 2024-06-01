2a3505c0ee121ff434626f005799b0269e3839d5
Gustavo Rebelo <gustavo.rblo@gmail.com>
2024-06-01 10:46:21 -0300

    Organizado

---
+++ EngenhariaDeSoftwareIII/Calculadora/src/Calculadora.java
@@ -0,0 +1,17 @@
+public class Calculadora {
+    public double soma(double a, double b){
+        return a+b;
+    }
+    public double diferenca(double a, double b){
+        return a-b;
+    }
+    public double divisao(double a, double b){
+        return a/b;
+    }
+    public double produto(double a, double b){
+        return a*b;
+    }
+
+
+
+}
