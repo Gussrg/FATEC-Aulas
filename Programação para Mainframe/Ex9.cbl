925f23e0fab41a4fee20e0fa5fa21a33773e669b
Gustavo Rebelo <134798105+Gussrg@users.noreply.github.com>
2024-08-30 11:39:18 -0300

    Desktob-COBOL

---
+++ Programação para Mainframe/Ex9.cbl
@@ -0,0 +1,88 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+      *****************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      *****************************************************************
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77 WRK-NUMERO        PIC 9(003)V9(001)  VALUE ZEROS.
+       77 WRK-RESTO         PIC 9(003)V9(001)  VALUE ZEROS.
+       77 WRK-ENTRADAPAR    PIC 9(003)         VALUE ZEROS.
+       77 WRK-ENTRADAIMPAR  PIC 9(003)         VALUE ZEROS.
+       77 WRK-CONTADOR      PIC 9(001)         VALUE ZEROS.
+       77 WRK-SAIDAPAR      PIC 9(003) OCCURS 5 TIMES  VALUE ZEROS.
+       77 WRK-SAIDAIMPAR    PIC 9(003) OCCURS 5 TIMES  VALUE ZEROS.
+       77 WRK-SOMAPAR       PIC 9(003)         VALUE ZEROS.
+       77 WRK-SOMAIMPAR     PIC 9(003)         VALUE ZEROS.
+       77 WRK-MAIORPAR      PIC 9(003)         VALUE ZEROS.
+       77 WRK-MAIORIMPAR    PIC 9(003)         VALUE ZEROS.
+       77 WRK-MENORPAR      PIC 9(003)         VALUE ZEROS.
+       77 WRK-MENORIMPAR    PIC 9(003)         VALUE ZEROS.
+
+       PROCEDURE DIVISION.
+       0001-PRINCIPAL.
+           PERFORM 0100-INICIALIZAR.
+
+
+       0100-INICIALIZAR.
+           DISPLAY "DIGITE O NUMERO DESEJADO"
+           ACCEPT WRK-NUMERO.
+       0200-PROCESSAR.
+
+       0300-FINALIZAR.
+
+
+
+      *VERIFICA SE O NÚMERO É PAR OU IMPAR
+
+           MOVE FUNCTION REM (WRK-NUMERO 2) TO WRK-RESTO.
+
+           IF WRK-RESTO = 0
+               MOVE WRK-NUMERO TO WRK-ENTRADAPAR
+               MOVE 1 TO WRK-CONTADOR
+           ELSE
+               MOVE WRK-NUMERO TO WRK-ENTRADAIMPAR
+           END-IF.
+
+           IF WRK-CONTADOR = 1
+               MOVE (WRK-ENTRADAPAR+2)  TO WRK-SAIDAPAR(1)
+               MOVE (WRK-SAIDAPAR(1)+2) TO WRK-SAIDAPAR(2)
+               MOVE (WRK-SAIDAPAR(2)+2) TO WRK-SAIDAPAR(3)
+               MOVE (WRK-SAIDAPAR(3)+2) TO WRK-SAIDAPAR(4)
+               MOVE (WRK-SAIDAPAR(4)+2) TO WRK-SAIDAPAR(5)
+
+           ELSE
+               MOVE (WRK-ENTRADAIMPAR+2)  TO WRK-SAIDAIMPAR(1)
+               MOVE (WRK-SAIDAIMPAR(1)+2) TO WRK-SAIDAIMPAR(2)
+               MOVE (WRK-SAIDAIMPAR(2)+2) TO WRK-SAIDAIMPAR(3)
+               MOVE (WRK-SAIDAIMPAR(3)+2) TO WRK-SAIDAIMPAR(4)
+               MOVE (WRK-SAIDAIMPAR(4)+2) TO WRK-SAIDAIMPAR(5)
+           END-IF.
+
+           MOVE (1) TO WRK-CONTADOR
+
+      *PARA MOSTRAR OS PRÓXIMOS 5 PARES APÓS O NÚMERO DIGITADO
+
+           PERFORM UNTIL WRK-CONTADOR = 5
+              DISPLAY WRK-SAIDAPAR(CONTADOR)
+              MOVE FUNCTION SUM(WRK-CONTADOR 1) TO WRK-CONTADOR
+           END-PERFORM.
+
+      *    MOVE (1) TO WRK-CONTADOR
+
+      **PARA MOSTRAR OS PRÓXIMOS 5 PARES APÓS O NÚMERO DIGITADO
+
+           PERFORM UNTIL WRK-CONTADOR = 5
+              DISPLAY WRK-SAIDAIMPAR(CONTADOR)
+              MOVE FUNCTION SUM(WRK-CONTADOR 1) TO WRK-CONTADOR
+           END-PERFORM.
+           STOP RUN.
+           END PROGRAM YOUR-PROGRAM-NAME.
