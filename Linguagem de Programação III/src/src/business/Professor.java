180f21392e1793a43af2190befd01e2a129e6c4b
Gustavo Rebelo Godoy <gustavo.rblo@gmail.com>
2025-03-11 20:31:06 -0300

    Notebook

---
+++ Linguagem de Programação III/src/src/business/Professor.java
@@ -0,0 +1,17 @@
+package business;
+
+public class Pessoa {
+	protected String nome, email;
+	
+	public String getNome() {return nome;}
+	//public void setNome(String nome {this.nome = nome;}
+	
+	publoc String getEmail() {return email;}
+		
+	public Pessoa(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+	}
+	
+	
+}
---
+++ Linguagem de Programação III/teste/src/cursoemvideo/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package cursoemvideo;
+
+import java.security.PublicKey;
+
+public class PrimeiroPrograma 
+{
+	
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Olá mundo!");
+		
+	
+	}
+}
