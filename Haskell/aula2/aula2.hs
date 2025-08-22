550b1d0ffe58673a82cc15475bd31acca65d972f
Gustavo Rebelo Godoy <gustavo.rblo@gmail.com>
2025-08-21 23:12:00 -0300

    Create aula2.hs

---
+++ Haskell/aula2/aula2.hs
@@ -0,0 +1,19 @@
+module Aula2 where
+
+dobro :: Int -> Int
+dobro x = 2*x
+
+tabuada :: Int -> [Int]
+tabuada k = [k*x | x <- [0 .. 10]]
+
+divisores :: Int -> [Int]
+divisores k = [x | x <-[1..k], mod k x == 0]
+
+ehPrimo :: Int -> Bool
+ehPrimo k = length (divisores k) == 2
+
+--Parametros2
+somar :: Int -> Int -> Int
+somar x y = x+y
+
+--Parametros2
